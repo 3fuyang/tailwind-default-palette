b0dce8055b0759609313196fbad0eca6ddc09ac0
3fuyang <fuyang@matrixorigin.cn>
2023-03-05 14:48:58 +0800

    feat: link to github repo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -79,14 +79,14 @@ export function App() {
   return (
     <div class="max-md:relative md:grid md:grid-cols-2">
       {/* Palette */}
-      <div class="mt-48 flex flex-col gap-2 py-4 md:col-start-1 md:mt-0 md:h-screen md:overflow-auto">
+      <div class="mt-52 flex flex-col gap-2 py-4 md:col-start-1 md:mt-0 md:h-screen md:overflow-auto">
         {/* Scheme Bar */}
         {colors.map(({ scheme, cells }) => (
           <div>
             {/* Scheme Title */}
             <h2
               id={scheme}
-              class="mb-2 font-semibold tracking-wide max-md:scroll-mt-48"
+              class="mb-2 font-semibold tracking-wide max-md:scroll-mt-56"
             >
               <a href={`#${scheme}`}>{scheme}</a>
             </h2>
@@ -116,7 +116,7 @@ export function App() {
       {/* Preview */}
       <div class="flex flex-col items-center justify-center py-4 max-md:fixed max-md:top-0 max-md:left-0 max-md:right-0 max-md:border-b max-md:bg-white md:col-start-2 md:h-screen">
         {/* Card */}
-        <div class="flex flex-col items-center justify-between gap-2 bg-white md:min-w-max md:gap-6 md:rounded-lg md:border md:py-6 md:px-10 md:shadow-xl">
+        <div class="flex flex-col items-center justify-between gap-2 bg-white md:min-w-max md:gap-4 md:rounded-lg md:border md:py-6 md:px-10 md:shadow-xl">
           {/* Theme Title */}
           <h2
             id="currThemeTitle"
@@ -179,6 +179,21 @@ export function App() {
               />
             ))}
           </div>
+          {/* External Links */}
+          <a
+            class="aspect-square w-6 text-slate-600 transition-colors hover:text-slate-500"
+            href="https://github.com/3fuyang/tailwind-default-palette"
+            target="_blank"
+            rel="noreferrer"
+            title="GitHub"
+          >
+            <svg viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34-.46-1.16-1.11-1.47-1.11-1.47-.91-.62.07-.6.07-.6 1 .07 1.53 1.03 1.53 1.03.87 1.52 2.34 1.07 2.91.83.09-.65.35-1.09.63-1.34-2.22-.25-4.55-1.11-4.55-4.92 0-1.11.38-2 1.03-2.71-.1-.25-.45-1.29.1-2.64 0 0 .84-.27 2.75 1.02.79-.22 1.65-.33 2.5-.33.85 0 1.71.11 2.5.33 1.91-1.29 2.75-1.02 2.75-1.02.55 1.35.2 2.39.1 2.64.65.71 1.03 1.6 1.03 2.71 0 3.82-2.34 4.66-4.57 4.91.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2z"
+              />
+            </svg>
+          </a>
         </div>
       </div>
     </div>
@@ -218,7 +233,6 @@ function getColorModel(source: string, type: 'HEX' | 'RGB' | 'CMYK' | 'HSL') {
   // Calculate the HSL values
   let h = -Infinity,
     s: number
-
   ;(r /= 255), (g /= 255), (b /= 255)
   const max = Math.max(r, g, b),
     min = Math.min(r, g, b)
